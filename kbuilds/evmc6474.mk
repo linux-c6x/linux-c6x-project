939f7ba8a037f6260b19c8025547398912a114b6
Sandeep Paulraj <s-paulraj@ti.com>
2010-08-26 01:38:23 -0400

    C6474: Adding config settings for Faraday
    Adding a file in the kbuilds directory for the C6474
    
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

---
+++ kbuilds/evmc6474.mk
@@ -0,0 +1,15 @@
+# these are mandatory
+LOCALVERSION = -evmc6474$(ENDIAN_SUFFIX)-$(DATE)
+DEFCONFIG = ti_evmc6474_defconfig
+
+# these are optional
+CONFIGPATCH =
+
+ifeq ($(ENDIAN),little)
+CMDLINE = console=cio ip=dhcp root=/dev/nfs nfsroot=158.218.100.25:/opt/min-root-c6x rw
+else
+CMDLINE = console=cio ip=dhcp root=/dev/nfs nfsroot=158.218.100.25:/opt/min-root-c6x-be rw
+endif
+
+# tack this on to name of kernel when copying vmlinux to product directory
+PRODVERSION = -1
