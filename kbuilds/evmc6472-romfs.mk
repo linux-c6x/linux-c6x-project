4c2689d5c3c635446a1ffb324c050741e2e1745e
Mark Salter <msalter@redhat.com>
2010-09-07 14:11:37 -0400

    Reuse previous build to speed up romfs variant build
    Reuse previous build to speed up romfs variant build
    
    Signed-off-by: Mark Salter <msalter@redhat.com>

--- kbuilds/evmc6472-romfs.mk
+++ kbuilds/evmc6472-romfs.mk
@@ -3,6 +3,7 @@ DEFCONFIG = ti_evmc6472_defconfig
 LOCALVERSION = -evmc6472$(ENDIAN_SUFFIX)-$(DATE)
 
 # these are optional
+KOBJNAME=evmc6472$(ENDIAN_SUFFIX)
 CONFIGPATCH =
 CMDLINE = console=cio ip=dhcp root=/dev/mtdblock0 ro
 PRODVERSION = -1-romfs
