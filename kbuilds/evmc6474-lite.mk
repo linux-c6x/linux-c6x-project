4e2b6d0ac3eb33c1690bac2b5873e730915e70c3
Sandeep Paulraj <s-paulraj@ti.com>
2010-09-23 18:12:18 -0400

    Add a entry for the Faraday Lite EVM
    This commit adds support for the Faraday Lite EVM
    to the c6x linux project eco system
    
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

---
+++ kbuilds/evmc6474-lite.mk
@@ -0,0 +1,17 @@
+# these are mandatory
+LOCALVERSION = -evmc6474_lite$(ENDIAN_SUFFIX)-$(DATE)
+DEFCONFIG = ti_evmc6474_lite_defconfig
+
+# these are optional
+CONFIGPATCH =
+
+ifeq ($(ENDIAN),little)
+CMDLINE = console=cio ip=dhcp root=/dev/nfs nfsroot=158.218.100.25:/opt/min-root-c6x rw
+#CMDLINE = console=cio ip=dhcp root=/dev/nfs nfsroot=10.218.100.248:/opt/min-root-c6474-le rw
+else
+CMDLINE = console=cio ip=dhcp root=/dev/nfs nfsroot=158.218.100.25:/opt/min-root-c6x-be rw
+#CMDLINE = console=cio ip=dhcp root=/dev/nfs nfsroot=10.218.100.248:/opt/min-root-c6474-be rw
+endif
+
+# tack this on to name of kernel when copying vmlinux to product directory
+PRODVERSION = -1
