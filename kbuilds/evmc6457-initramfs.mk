256d579399a57965daa9fae83bbc9c203146d4f5
Sandeep Paulraj <s-paulraj@ti.com>
2010-11-10 10:15:36 -0500

    EVM c6457: Adding a config for initramfs kernel
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

---
+++ kbuilds/evmc6457-initramfs.mk
@@ -0,0 +1,12 @@
+# these are mandatory
+DEFCONFIG = ti_evmc6457_defconfig
+LOCALVERSION = -evmc6457$(ENDIAN_SUFFIX)-$(DATE)
+
+# these are optional
+KOBJNAME=evmc6457$(ENDIAN_SUFFIX)
+CONFIGPATCH =
+CONFIGSCRIPT = initramfs.sh
+CONFIGARGS = $(BLD)/rootfs/$(ROOTFS)-$(ARCHe) NONE
+
+CMDLINE = console=ttySI0,115200 ip=dhcp rw
+PRODVERSION = -1-initramfs
