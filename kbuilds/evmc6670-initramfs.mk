9c94accf40cddfa6673ca22bb9717b388c121c46
Sandeep Paulraj <s-paulraj@ti.com>
2011-04-15 15:59:17 -0400

    6670: Update the build config
    Signed-off-by: Sandeep Paulraj <s-paulraj@ti.com>

--- kbuilds/evmc6670-initramfs.mk
+++ kbuilds/evmc6670-initramfs.mk
@@ -8,5 +8,5 @@ CONFIGPATCH =
 CONFIGSCRIPT = initramfs.sh
 CONFIGARGS = $(BLD)/rootfs/$(ROOTFS)-$(ARCHe) NONE
 
-CMDLINE = console=cio rw
+CMDLINE = console=cio ip=dhcp rw
 PRODVERSION = -1-initramfs
