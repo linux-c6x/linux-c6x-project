ea000339fddf5c65a2c989b019a467b3735a442c
Aurelien JACQUIOT-XID <x0134876@lnx0134876.tif.ti.com>
2011-07-07 17:28:59 +0200

    Fixed multi-Linux profiles for C6472 and C6474
     - Update kernel features for recent kernel updates
     - Add Virtio support when using multi-Linux profiles
    
    Signed-off-by: Aurelien JACQUIOT-XID <x0134876@lnx0134876.tif.ti.com>

--- kbuilds/evmc6472-A.mk
+++ kbuilds/evmc6472-A.mk
@@ -3,7 +3,7 @@ LOCALVERSION = -evmc6472$(ENDIAN_SUFFIX)-$(DATE)-A
 DEFCONFIG = ti_evmc6472_defconfig
 
 # these are optional
-CONFIGPATCH =
+CONFIGPATCH = patches/ti_evmc6472_A_defconfig.patch
 
 ifeq ($(ENDIAN),little)
 CMDLINE = console=cio ip=dhcp root=/dev/nfs nfsroot=158.218.100.25:/opt/min-root-c6x-nov mem=32M rw
--- kbuilds/evmc6474-A.mk
+++ kbuilds/evmc6474-A.mk
@@ -3,7 +3,7 @@ LOCALVERSION = -evmc6474$(ENDIAN_SUFFIX)-$(DATE)-A
 DEFCONFIG = ti_evmc6474_defconfig
 
 # these are optional
-CONFIGPATCH =
+CONFIGPATCH = patches/ti_evmc6474_A_defconfig.patch
 
 ifeq ($(ENDIAN),little)
 CMDLINE = console=cio ip=dhcp root=/dev/nfs nfsroot=158.218.100.25:/opt/min-root-c6x-nov mem=32M rw
