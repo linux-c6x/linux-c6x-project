5d93daf472d671a5ee6aa7d078a1b20ab08f9a66
Bill Mills <wmills@ti.com>
2011-07-25 17:02:00 -0400

    bootblobs: add a set of bootblobs for evmc6670
    Signed-off-by: Bill Mills <wmills@ti.com>

---
+++ bootblob-specs/evmc6670-nfs.mk
@@ -0,0 +1,7 @@
+# makefile fragment to create a specific bootblob
+
+EVM=evmc6670
+INITRAMFS=
+ROOT=root=/dev/nfs nfsroot=/sysroots/$(EVM)$(ENDIAN_SUFFIX)/,v3,tcp rw
+
+include $(PRJ)/bootblob-specs/defs/evmc667x.mk
