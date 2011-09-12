ceef0b2f62ac2c75b76d93b2d92a28443d52893a
Bill Mills <wmills@ti.com>
2011-09-12 15:10:50 -0400

    release-build: include syslink-all in ltp bundles
    Need to include syslink-all in ltp bundles (we don't want it in the product bundles)
    So we must build each kernel that uses syslink
    But, we don't want to publish two kernels for each EVM
    so we delete them from the bundle before tar'ing
    
    Added an optional pre-<config.sh and post-<config>.sh capability to release-build
    
    Signed-off-by: Bill Mills <wmills@ti.com>

---
+++ scripts/release-build/post-ltpbe.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# post build script for release-build
+
+# for LTP release bundles, the kernel builds are needed to syslink-all but
+# should not be included in the release bundle
+rm ../product/vmlinux* ../product/modules*
+
